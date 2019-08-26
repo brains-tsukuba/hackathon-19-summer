43543f04bcdfd883b85d7f5767a5b343e2316922
KoyamaSohei <koyamaso0309@gmail.com>
2019-08-26 12:31:29 +0900

    update Co-authored-by: HaraguchiMasahiro <momochan5454@gmail.com> Co-authored-by: Keiichi Sato <kei1satoh0603@gmail.com> Co-authored-by: HikaruEgashira <shighten@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>めしつくば</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <router-link tag="a" to="/about">about</router-link>
+      <router-link tag="a" to="/review">review</router-link>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+
+export default {
+  name: 'App',
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  a{
+    text-decoration:none;
+    color:#000;
+    margin:0 10px;
   }
-}
 </style>
