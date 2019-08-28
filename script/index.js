8030d832acf5157df5afe2a97d88acbf4fe21772
KoyamaSohei <koyamaso0309@gmail.com>
2019-08-29 07:30:15 +0900

    refactor

--- script/index.js
+++ script/index.js
@@ -12,29 +12,33 @@ function generateSerial(len){
   return serial
 }
 
+const promises = [];
+
 let output = [];
 for(let i = 1; i <= 54;i++) {
-  axios.get(`https://tsukumeshi.herokuapp.com/result?keyword=&page=${i}&utf8=%E2%9C%93`)
+  promises.push(new Promise((resolve,reject) => {
+    axios.get(`https://tsukumeshi.herokuapp.com/result?keyword=&page=${i}&utf8=%E2%9C%93`)
   .then((res) => {
     const regrep = /\[\{"id"([\s\S]*?)]/g;
     const re = new RegExp(regrep)
     output = output.concat(JSON.parse(res.data.match(re)[0]))
-    if(i == 54) {
-      const out = output.reduce((acc,cur) => {
-        acc[generateSerial(20)] = {
-            name: cur.name,
-            latitude: cur.latitude,
-            longitude: cur.longitude
-        }
-        return acc
-      },{});
-      
-      const data = new Uint8Array(Buffer.from(JSON.stringify(out)));
-      fs.writeFile('message.json', data, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-      });
-    }
-  })
-  .catch((err) => console.log(err))
+    resolve();
+    }).catch((e) => reject(e))
+  }));
 }
+Promise.all(promises).then(() => {
+  const out = output.reduce((acc,cur) => {
+    acc[generateSerial(20)] = {
+        name: cur.name,
+        latitude: cur.latitude,
+        longitude: cur.longitude
+    }
+    return acc
+  },{});
+  
+  const data = new Uint8Array(Buffer.from(JSON.stringify(out)));
+  fs.writeFile('message.json', data, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+}).catch((e) => console.log(e))
