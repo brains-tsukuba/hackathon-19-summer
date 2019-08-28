079f9916f465bb183e096dca121f3e621945de8f
KoyamaSohei <koyamaso0309@gmail.com>
2019-08-28 14:48:29 +0900

    tweet after post

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,7 +1,9 @@
 <template>
   <v-container fluid>
-    <v-layout>
-      <v-flex xs10 offset-xs1>
+    <v-layout class="post-vue">
+      <v-flex xs10 offset-xs1 class="wrapper">
+        
+        <div class="shop_select"> 
         <v-row align="center">
           <v-col class="d-flex" cols="12" sm="6">
             <v-select
@@ -11,16 +13,18 @@
             ></v-select>
           </v-col>
         </v-row>
-        <div class="my-2">
-          <v-btn large @click="searchNearStore()">近くのお店を検索</v-btn>
+        <div class="my-2 button_justify">
+          <v-btn large @click="searchNearStore()" class="search_button">ちかくをけんさく</v-btn>
+        </div>
         </div>
          <v-card
             class="mx-auto my-2"
             tile
+            v-if="visible"
           >
-            <v-list-item v-for="(shop, id) in nearShop" :key="id" ripple @click="shopName = shop[1].name">
+            <v-list-item v-for="(shop, id) in nearShop" :key="id" ripple @click="shopName = shop[1].name; visible=false">
               <v-list-item-content>
-                <v-list-item-title>{{ shop[1].name }}</v-list-item-title>
+                <v-list-item-title class="list">{{ shop[1].name }}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
          </v-card>
@@ -29,15 +33,17 @@
           label="Review"
           auto-grow
           v-model="reviewText"
-          placeholder="お店に行った感想を入力してください。"
+          class="textarea"
         />
-        <div class="my-2">
-          <div v-if="!authenticated" >ログインしてください</div>
+        <div v-if="!authenticated" class="login">ログインしてね</div>
+        <div class="button_justify">
           <v-btn large @click="handleSubmit()" :disabled="!ableSubmit">投稿</v-btn>
+          <v-checkbox color="primary" v-model="tweet" label="Twitterにも投稿する"></v-checkbox>
         </div>
       </v-flex>
     </v-layout>
   </v-container>
+  
 </template>
 <script>
 import { mapGetters } from 'vuex'
@@ -46,6 +52,8 @@ import { db } from '@/main'
 export default {
   data: function() {
     return {
+      tweet: true,
+      visible: true,
       reviewText: '',
       shop: {},
       nearShop: [],
@@ -62,6 +70,7 @@ export default {
   },
   methods: {
     async searchNearStore() {
+      this.visible = true;
       const {latitude, longitude} = await this.initGeolocationSearch();
       const res = Object.entries(this.shop).filter(([key, value]) => {
         const diff = Math.pow(latitude - value.latitude,2) + Math.pow(longitude - value.longitude,2) < 0.00005
@@ -94,14 +103,13 @@ export default {
         console.err('データが取得できませんでした。電波の届きやすい場所で再度お試しください。',);
       });
     },
-    handleSubmit() {
+    async handleSubmit() {
       const reviewRef = db.ref('review')
       const shopName = this.shopName
       const shop = this.shop
       const content = this.reviewText
       const date = new Date()
-      const ymd = `${date.getFullYear()}-${date.getMonth() +
-        1}-${date.getDate()}`
+      const ymd = `${date.getFullYear()}-${("0"+(date.getMonth() + 1)).slice(-2)}-${("0"+date.getDate()).slice(-2)}`
       const key = Object.keys(shop).find((key) => shop[key].name === shopName)
       const newRef = reviewRef.push()
       newRef.set({
@@ -110,6 +118,10 @@ export default {
         date: ymd,
         username: this.username
       })
+      const tweet = `${shopName}のレビューを投稿しました。レビュー内容はこちら！ https://meshitsukuba.web.app/review/${key}/${newRef.key}`;
+      if (this.tweet) {
+        await fetch(`https://script.google.com/macros/s/AKfycbzO8YEvSj_CpVWtknt61y3BA7Mz58ut2WEcVRV3uDVzxQ0W5K-u/exec?at=${this.accessToken}&sc=${this.secretToken}&ct=${encodeURIComponent(tweet)}`)
+      }
       this.$router.push(`/review/${key}/${newRef.key}`)
     },
   },
@@ -122,14 +134,66 @@ export default {
     },
     ...mapGetters('auth', {
       authenticated: "authenticated",
-      username: "username"
+      username: "username",
+      accessToken: "accessToken",
+      secretToken: "secretToken"
     })
   }
 }
 </script>
 <style lang="scss">
+
+label {
+  font-family: arial !important;
+}
 .v-input__control {
   margin: 0 auto;
 }
 
+.shop_select{
+  border: solid 2px #999;
+  margin-top:30px;
+  border-radius: 0.5rem;
+  margin-bottom:40px;
+}
+
+
+h2{
+  margin:0 auto;
+}
+.d-flex{
+  margin:0 auto;
+}
+.button_justify{
+  text-align: center;
+}
+.v-list-item__title{
+  font-family:Roboto;
+}
+.login{
+  text-align:center;
+  margin:20px 0;
+}
+
+textarea{
+  margin-top:100px
+}
+.wrapper{
+  border-radius:4px;
+  background-color:#fff;
+  max-width:650px;
+  margin:0 auto;
+  padding:40px 40px;
+  margin-top:60px;
+}
+textarea{
+  font-family:Roboto !important;
+}
+.v-select__selection{
+  font-family:Roboto;
+}
+
+.post-vue{
+  background-color:"primary";
+}
 </style>
